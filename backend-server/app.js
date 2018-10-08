a0b77cd47d39f0ceeda64ff6219fe104e5ba3cc6
mflores10 <itsg.mflores1@gmail.com>
2018-10-08 07:44:55 -0600

    Primer commit

--- backend-server/app.js
+++ backend-server/app.js
@@ -1,11 +1,25 @@
 // Requires
 var express = require('express');
-
+var mongoose = require('mongoose');
 
 // Inicializar variables
 var app = express();
 
+// Conexión a la base de datos
+mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) => {
+    if (err) throw err;
 
+    console.log('Base de datos: \x1b[32m%s\x1b[0m', 'on line');
+})
+
+
+// Rutas
+app.get('/', (req, res, next) => {
+    res.status(200).json({
+        ok: true,
+        mensaje: 'Petición realizada correctamente'
+    });
+});
 
 // Escuchar peticiones
 app.listen(3000, () => {
